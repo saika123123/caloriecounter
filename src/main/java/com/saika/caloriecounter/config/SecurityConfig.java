26b25d62c743fe2a2550e8af27bdf048e863b790
saika123123 <saika1154ok@gmail.com>
2024-05-13 19:28:57 +0900

    re2

--- src/main/java/com/saika/caloriecounter/config/SecurityConfig.java
+++ src/main/java/com/saika/caloriecounter/config/SecurityConfig.java
@@ -21,15 +21,18 @@ public class SecurityConfig { // セキュリティ設定のクラス
     @Bean // このメソッドの返り値をSpringのBeanとして登録します
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // セキュリティフィルタチェーンを定義するメソッド
         http
-            .formLogin(formLogin ->  // フォームベースのログインを設定します
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/login", "/register").permitAll()
+                        .anyRequest().authenticated()) // "/login"と"/register"は認証なしでアクセス可能です。それ以外のリクエストは認証が必要です
+                .formLogin(formLogin -> // フォームベースのログインを設定します
                 formLogin
-                    .loginPage("/login") // ログインページのURLを設定します
-                    .permitAll() // ログインページは認証なしで許可します
-            )
-            .logout(logout ->  // ログアウトを設定します
+                        .loginPage("/login") // ログインページのURLを設定します
+                        .permitAll() // ログインページは認証なしで許可します
+                )
+                .logout(logout -> // ログアウトを設定します
                 logout
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // ログアウトのリクエストURLを設定します
-            );
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // ログアウトのリクエストURLを設定します
+                );
 
         return http.build(); // 上記の設定を反映してHttpSecurityオブジェクトをビルドします
     }
